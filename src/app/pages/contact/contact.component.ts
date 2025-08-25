5286df05487f640878497e2214870937ca4d9c72
Gema Rubio <gemarubio89@gmail.com>
2025-08-25 02:15:48 +0200

    agrego contactme

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+ name: string = 'Gema Rubio Sánchez';
+  title: string = 'Full-Stack Developer';
 
+  contactInfo = [
+    { icon: '📧', type: 'Email', value: 'gemarubio@gmail.com' },
+    { icon: '💼', type: 'LinkedIn', value: 'linkedin.com/in/gema-rubio-sanchez', link: 'https://www.linkedin.com/in/gema-rubio-sanchez/' },
+    { icon: '📞', type: 'Teléfono', value: '697411247' }
+  ];
 }
